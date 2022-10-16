97446b5429d2f6c8902cda85f7ab63fcb14a44ed
Aliaksei Syrel <alex.syrel@gmail.com>
2022-10-16 12:53:52 +0200

    update runner script

--- rust-worker/start-rust-worker-aarch64.ps1
+++ rust-worker/start-rust-worker-aarch64.ps1
@@ -1,5 +1,6 @@
 $env:RUST_LOG="info"
 
-foreach ($i in 1..500) {
+foreach ($i in 1..960) {
+	Start-Sleep -Milliseconds 20
     Start-Process -NoNewWindow -FilePath "target\aarch64-pc-windows-msvc\release\rust-worker.exe"
 }
